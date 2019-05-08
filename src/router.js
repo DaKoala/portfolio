235d1c68490c0dc4755134ca8f23e4c496183566
billyzou <yz3352@nyu.edu>
2019-05-08 13:02:38 -0400

    feat(router): change router mode from hash to history

--- src/router.js
+++ src/router.js
@@ -11,6 +11,7 @@ const isLocalhost = window.location.hostname === 'localhost' ||
     window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/);
 
 const router = new Router({
+    mode: 'history',
     routes: [
         {
             path: '/',
